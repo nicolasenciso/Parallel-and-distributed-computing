55574854933768992b0789227524950ea11e252d
Nicolas Enciso <nricardoe@unal.edu.co>
2019-04-30 04:15:33 +0000

    Add files via upload
    added while to run all kernel an threads number

--- practica1/script_ejecutar_todo.sh
+++ practica1/script_ejecutar_todo.sh
@@ -1,28 +1,44 @@
+#!/bin/bash
 g++ blur-effect.cpp -o y `pkg-config --cflags --libs opencv` -lpthread
 
 printf "\n --- HD blur processing --- \n"
-for i in {1..16}
-do
-    for j in {3..15}
+threads=1
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    while [ $threads -le 16 ]
     do
-    ./y hdLancia.jpg blur/hd-blur.jpg $j $i | tee -a times/hd-times.in
+        ./y hdLancia.jpg blur/hd-blur.jpg $kernelsize $threads | tee -a times/hd-times.in
+        threads=$((threads+1))
     done
+    kernelsize=$((kernelsize+1))
 done
 
- printf "\n --- Full HD blur processing --- \n"
- for i in {1..16}
- do
-    for j in {3..15}
-    do
-     ./y fhdLancia.jpg blur/full_hd-blur.jpg $j $i | tee -a times/fhd-times.in
-     done
- done
+printf "\n --- FHD blur processing --- \n"
+threads=1
+kernelsize=3
 
- printf "\n --- 4K blur processing--- \n"
- for i in {1..16}
- do
-    for j in {3..15}
+while [ $kernelsize -le 15 ]
+do  
+    while [ $threads -le 16 ]
     do
-     ./y 4kLancia.jpg blur/4k-blur.jpg $j $i | tee -a times/4k-times.in
+        ./y fhdLancia.jpg blur/full_hd-blur.jpg $kernelsize $threads | tee -a times/fhd-times.in
+        threads=$((threads+1))
     done
- done
+    kernelsize=$((kernelsize+1))
+done
+
+printf "\n --- 4K blur processing --- \n"
+threads=1
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    while [ $threads -le 16 ]
+    do
+        ./y 4kLancia.jpg blur/4k-blur.jpg $kernelsize $threads | tee -a times/4k-times.in
+        threads=$((threads+1))
+    done
+    kernelsize=$((kernelsize+1))
+done
