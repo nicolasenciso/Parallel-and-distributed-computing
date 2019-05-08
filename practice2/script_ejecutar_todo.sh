c5a65026f00a182dc51dad356578c621006fe681
nricardoe <nricardoe@unal.edu.co>
2019-05-07 23:29:50 -0500

    added shellscript for exe

---
+++ practice2/script_ejecutar_todo.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+g++ blur-effect.cpp -o y `pkg-config --cflags --libs opencv` -lpthread
+
+printf "\n --- HD blur processing --- \n"
+threads=1
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y hdLancia.jpg blur/hd-blur.jpg $kernelsize $threads | tee -a times/hd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=2
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y hdLancia.jpg blur/hd-blur.jpg $kernelsize $threads | tee -a times/hd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=4
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y hdLancia.jpg blur/hd-blur.jpg $kernelsize $threads | tee -a times/hd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=8
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y hdLancia.jpg blur/hd-blur.jpg $kernelsize $threads | tee -a times/hd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=16
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y hdLancia.jpg blur/hd-blur.jpg $kernelsize $threads | tee -a times/hd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+
+printf "\n --- FHD blur processing --- \n"
+threads=1
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y fhdLancia.jpg blur/full_hd-blur.jpg $kernelsize $threads | tee -a times/fhd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=2
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y fhdLancia.jpg blur/full_hd-blur.jpg $kernelsize $threads | tee -a times/fhd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=4
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y fhdLancia.jpg blur/full_hd-blur.jpg $kernelsize $threads | tee -a times/fhd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=8
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y fhdLancia.jpg blur/full_hd-blur.jpg $kernelsize $threads | tee -a times/fhd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+threads=16
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y fhdLancia.jpg blur/full_hd-blur.jpg $kernelsize $threads | tee -a times/fhd-times.in    
+    kernelsize=$((kernelsize+1))
+done
+        
+
+printf "\n --- 4K blur processing --- \n"
+threads=1
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y 4kLancia.jpg blur/4k-blur.jpg $kernelsize $threads | tee -a times/4k-times.in
+    kernelsize=$((kernelsize+1))
+done
+threads=2
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y 4kLancia.jpg blur/4k-blur.jpg $kernelsize $threads | tee -a times/4k-times.in
+    kernelsize=$((kernelsize+1))
+done
+threads=4
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y 4kLancia.jpg blur/4k-blur.jpg $kernelsize $threads | tee -a times/4k-times.in
+    kernelsize=$((kernelsize+1))
+done
+threads=8
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y 4kLancia.jpg blur/4k-blur.jpg $kernelsize $threads | tee -a times/4k-times.in
+    kernelsize=$((kernelsize+1))
+done
+threads=16
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./y 4kLancia.jpg blur/4k-blur.jpg $kernelsize $threads | tee -a times/4k-times.in
+    kernelsize=$((kernelsize+1))
+done
