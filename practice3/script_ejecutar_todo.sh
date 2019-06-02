f73e1db84b4b828563bb8368236faa3f6a3462ef
Nicolas Enciso <nricardoe@unal.edu.co>
2019-06-02 01:09:09 -0500

    from 1 to 15 and 1 to 100 loop kernel size

--- practice3/script_ejecutar_todo.sh
+++ practice3/script_ejecutar_todo.sh
@@ -35,7 +35,36 @@ done
 
 
 
+printf "\n --- HD blur processing --- \n"
 
+kernelsize=3
+
+while [ $kernelsize -le 100 ]
+do  
+    ./gauss_cuda hdLancia.png blur/hdLanciaBlur.png $kernelsize | tee -a times/hd-times.txt    
+    kernelsize=$((kernelsize+1))
+done
+
+printf "\n --- FHD blur processing --- \n"
+
+kernelsize=3
+
+while [ $kernelsize -le 100 ]
+do  
+    ./gauss_cuda fhdLancia.png blur/fhdLanciaBlur.png $kernelsize | tee -a times/fhd-times.txt    
+    kernelsize=$((kernelsize+1))
+done
+
+
+printf "\n --- 4k blur processing --- \n"
+
+kernelsize=3
+
+while [ $kernelsize -le 100 ]
+do  
+    ./gauss_cuda 4kLancia.png blur/4kLanciaBlur.png $kernelsize | tee -a times/4k-times.txt    
+    kernelsize=$((kernelsize+1))
+done
 
 
 
