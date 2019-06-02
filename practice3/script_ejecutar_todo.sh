87bc69f15ba2905ae4d4b56fece4fe2c047c569c
Nicolas Enciso <nricardoe@unal.edu.co>
2019-06-02 00:46:17 -0500

    Add files via upload

---
+++ practice3/script_ejecutar_todo.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+nvcc gauss_cuda.cu -o gauss_cuda -I /usr/local/cuda/samples/common/inc/ -lpng
+
+printf "\n --- HD blur processing --- \n"
+
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./gauss_cuda hdLancia.png blur/hdLanciaBlur.png $kernelsize | tee -a times/hd-times.txt    
+    kernelsize=$((kernelsize+1))
+done
+
+printf "\n --- FHD blur processing --- \n"
+
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./gauss_cuda fhdLancia.png blur/fhdLanciaBlur.png $kernelsize | tee -a times/fhd-times.txt    
+    kernelsize=$((kernelsize+1))
+done
+
+
+printf "\n --- 4k blur processing --- \n"
+
+kernelsize=3
+
+while [ $kernelsize -le 15 ]
+do  
+    ./gauss_cuda 4kLancia.png blur/4kLanciaBlur.png $kernelsize | tee -a times/4k-times.txt    
+    kernelsize=$((kernelsize+1))
+done
+
+
+
+
+
+
+
+
+
