bae04dfc1652ff0b85c86fefe6e20e6671258a9b
Nicolas Enciso <nricardoe@unal.edu.co>
2019-06-02 01:10:28 -0500

    1 to 15 and 1 to 100 kernel sizes on different txts

--- practice3/script_ejecutar_todo.sh
+++ practice3/script_ejecutar_todo.sh
@@ -41,7 +41,7 @@ kernelsize=3
 
 while [ $kernelsize -le 100 ]
 do  
-    ./gauss_cuda hdLancia.png blur/hdLanciaBlur.png $kernelsize | tee -a times/hd-times.txt    
+    ./gauss_cuda hdLancia.png blur/hdLanciaBlur.png $kernelsize | tee -a times/hd-times100.txt    
     kernelsize=$((kernelsize+1))
 done
 
@@ -51,7 +51,7 @@ kernelsize=3
 
 while [ $kernelsize -le 100 ]
 do  
-    ./gauss_cuda fhdLancia.png blur/fhdLanciaBlur.png $kernelsize | tee -a times/fhd-times.txt    
+    ./gauss_cuda fhdLancia.png blur/fhdLanciaBlur.png $kernelsize | tee -a times/fhd-times100.txt    
     kernelsize=$((kernelsize+1))
 done
 
@@ -62,7 +62,7 @@ kernelsize=3
 
 while [ $kernelsize -le 100 ]
 do  
-    ./gauss_cuda 4kLancia.png blur/4kLanciaBlur.png $kernelsize | tee -a times/4k-times.txt    
+    ./gauss_cuda 4kLancia.png blur/4kLanciaBlur.png $kernelsize | tee -a times/4k-times100.txt    
     kernelsize=$((kernelsize+1))
 done
 
