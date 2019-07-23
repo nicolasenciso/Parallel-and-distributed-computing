05ea977920747bb83249e6b743349c69546875fc
Nicolas Enciso <nricardoe@unal.edu.co>
2019-07-23 04:42:25 -0500

    Add files via upload

--- parcial2/cuda/matrixMult.cu
+++ parcial2/cuda/matrixMult.cu
@@ -21,48 +21,69 @@
  return: none
  *********************************************************************
  */
- __global__ void gpu_matrix_mult(int *a,int *b, int *c, int m, int n, int k)
- { 
-     int row = blockIdx.y * blockDim.y + threadIdx.y; 
-     int col = blockIdx.x * blockDim.x + threadIdx.x;
-     int sum = 0;
-     if( col < k && row < m) 
-     {
-         for(int i = 0; i < n; i++) 
-         {
-             sum += a[row * n + i] * b[i * k + col];
-         }
-         c[row * k + col] = sum;
-     }
- } 
- 
  /*
- *********************************************************************
- function name: gpu_matrix_transpose
- description: matrix transpose
- parameters: 
-             &mat_in GPU device pointer to a rows X cols matrix
-             &mat_out GPU device output purpose pointer to a cols X rows matrix 
-             to store the result
- Note:
-     grid and block should be configured as:
-         dim3 dim_grid((n - 1) / BLOCK_SIZE + 1, (n - 1) / BLOCK_SIZE + 1, 1);
-         dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
- return: none
- *********************************************************************
- */
- __global__ void gpu_matrix_transpose(int* mat_in, int* mat_out, unsigned int rows, unsigned int cols) 
- {
-     unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
-     unsigned int idy = blockIdx.y * blockDim.y + threadIdx.y;
+*********************************************************************
+function name: gpu_square_matrix_mult
+description: dot product of two matrix (not only square) in GPU
+parameters: 
+            &a GPU device pointer to a n X n matrix (A)
+            &b GPU device pointer to a n X n matrix (B)
+            &c GPU device output purpose pointer to a n X n matrix (C) 
+            to store the result
+Note:
+    grid and block should be configured as:
+        dim3 dim_grid((n - 1) / BLOCK_SIZE + 1, (n - 1) / BLOCK_SIZE + 1, 1);
+        dim3 dim_block(BLOCK_SIZE, BLOCK_SIZE, 1);
+return: none
+*********************************************************************
+*/
+__global__ void gpu_square_matrix_mult(int *d_a, int *d_b, int *d_result, int n) 
+{
+    __shared__ int tile_a[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ int tile_b[BLOCK_SIZE][BLOCK_SIZE];
+
+    int row = blockIdx.y * BLOCK_SIZE + threadIdx.y;
+    int col = blockIdx.x * BLOCK_SIZE + threadIdx.x;
+    int tmp = 0;
+    int idx;
+
+    for (int sub = 0; sub < gridDim.x; ++sub) 
+    {
+        idx = row * n + sub * BLOCK_SIZE + threadIdx.x;
+        if(idx >= n*n)
+        {
+            // n may not divisible by BLOCK_SIZE
+            tile_a[threadIdx.y][threadIdx.x] = 0;
+        }
+        else
+        {
+            tile_a[threadIdx.y][threadIdx.x] = d_a[idx];
+        }
+
+        idx = (sub * BLOCK_SIZE + threadIdx.y) * n + col;
+        if(idx >= n*n)
+        {
+            tile_b[threadIdx.y][threadIdx.x] = 0;
+        }  
+        else
+        {
+            tile_b[threadIdx.y][threadIdx.x] = d_b[idx];
+        }
+        __syncthreads();
+
+        for (int k = 0; k < BLOCK_SIZE; ++k) 
+        {
+            tmp += tile_a[threadIdx.y][k] * tile_b[k][threadIdx.x];
+        }
+        __syncthreads();
+    }
+    if(row < n && col < n)
+    {
+        d_result[row * n + col] = tmp;
+    }
+}
+
  
-     if (idx < cols && idy < rows) 
-     {
-         unsigned int pos = idy * cols + idx;
-         unsigned int trans_pos = idx * rows + idy;
-         mat_out[trans_pos] = mat_in[pos];
-     }
- }
  /*
  *********************************************************************
  function name: cpu_matrix_mult
@@ -154,7 +175,7 @@
      dim3 dimGrid(grid_cols, grid_rows);
      dim3 dimBlock(BLOCK_SIZE*atoi(argv[2]), BLOCK_SIZE*atoi(argv[2]));
     
-     gpu_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, m, n, k);   
+     gpu_square_matrix_mult<<<dimGrid, dimBlock>>>(d_a, d_b, d_c, n);
 
      // Transefr results from device to host 
      cudaMemcpy(h_c, d_c, sizeof(int)*m*k, cudaMemcpyDeviceToHost);
@@ -165,7 +186,7 @@
  
      // compute time elapse on GPU computing
      cudaEventElapsedTime(&gpu_elapsed_time_ms, start, stop);
-     printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on GPU: %f ms.\n\n", m, n, n, k, gpu_elapsed_time_ms);
+    //  printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on GPU: %f ms.\n\n", m, n, n, k, gpu_elapsed_time_ms);
  
      // start the CPU version
      cudaEventRecord(start, 0);
@@ -175,7 +196,7 @@
      cudaEventRecord(stop, 0);
      cudaEventSynchronize(stop);
      cudaEventElapsedTime(&cpu_elapsed_time_ms, start, stop);
-     printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on CPU: %f ms.\n\n", m, n, n, k, cpu_elapsed_time_ms);
+     //printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on CPU: %f ms.\n\n", m, n, n, k, cpu_elapsed_time_ms);
  
      // validate results computed by GPU
      int all_ok = 1;
@@ -195,7 +216,8 @@
      // roughly compute speedup
      if(all_ok)
      {
-         printf("all results are correct!!!, speedup = %f\n", cpu_elapsed_time_ms / gpu_elapsed_time_ms);
+        //  printf("all results are correct!!!, speedup = %f\n", cpu_elapsed_time_ms / gpu_elapsed_time_ms);
+         printf("%d\t%d\t%f\n",n,atoi(argv[2]), cpu_elapsed_time_ms / gpu_elapsed_time_ms);
      }
      else
      {
